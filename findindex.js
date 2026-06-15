90b905a56381afa103dbe0e08cf6ac300645400a
Thowhidul Mehraj seyam <sseyam450@gmail.com>
2026-06-15 23:44:08 +0600

    Delete findindex.js

--- findindex.js
+++
@@ -1,8 +0,0 @@
-function findindex(arr , element) {
-    return arr.indexOf(element);
-
-}
-
-
-console.log(findindex([ 1 , 2 , 3 , 4 , 5], 3))
-console.log(findindex(["sifat","amit","nude"],"karim"))
